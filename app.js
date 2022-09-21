4522fb6113f399d94197ff861962e5e0e3417bd9
Shahed hossen <shahedhossensaad@gmail.com>
2022-09-22 01:12:15 +0600

    Added Database

--- app.js
+++ app.js
@@ -1,34 +1,91 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const mongoose = require("mongoose");
+const _ = require("lodash");
 const app = express();
-var inputs = [];
+
+mongoose.connect("mongodb+srv://admin:admin123@cluster0.6uv7ni8.mongodb.net/todolistDB");
+
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
-    const today = new Date();
-  
-   var option = {
-    weekday : "long",
-    day : "numeric",
-    month : "long"
-   };
+const todoSchema = new mongoose.Schema({
+  name: String,
+});
+const item = mongoose.model("item", todoSchema);
 
-   var day = today.toLocaleDateString("en-us", option);
+app.get("/", function (req, res) {
+  item.find({}, (err, items) => {
+    res.render("list", { ListTitle: "Today", listeditem: items });
+  });
+});
 
-    res.render("list", {ListTitle:day, listeditem:inputs});
-})
+const listSchema = new mongoose.Schema({
+  name: String,
+  list: [todoSchema]
+});
 
-app.post("/", function(req, res){
-    const userinput = req.body.input;
-    inputs.push(userinput);
+const list = mongoose.model("list", listSchema);
+
+
+app.get("/:newpost", (req, res) => {
+  const newpage = _.capitalize(req.params.newpost);
+    if(newpage === "Favicon.ico"){
+        list.deleteMany({name:"Favicon.ico"},(err)=>{});
+    }
+  list.findOne({ name: newpage }, (err, items) => {
+    if (!err) {
+      if (!items) {
+        const newlist = new list({
+          name: newpage,
+          list: []
+        });
+        newlist.save();
+        res.redirect("/" + newpage);
+      } else {
+        res.render("list", { ListTitle: items.name, listeditem: items.list });
+      }
+    }
+  });
+});
+
+app.post("/delete", (req, res) => {
+  const checked = req.body.checkbox;
+  const listname = _.capitalize(req.body.listname);
+
+  if(listname === "Today"){
+    item.deleteOne({ _id: checked }, (err) => {
+        res.redirect("/");
+      });
+  }
+  else{
+    list.findOneAndUpdate({name: listname},{$pull:{list:{_id:checked}}}, (err, deletedItem)=>{
+         res.redirect("/"+ listname);
+    })
+  }
+});
+
+app.post("/", (req, res) => {
+  const userinput = req.body.input;
+  const listname = req.body.submit;
+
+  const inputs = new item({
+    name: userinput,
+  });
+
+  if (listname === "Today") {
+    inputs.save();
     res.redirect("/");
-   
-})
+  } else {
+    list.findOne({ name: listname }, (err, foundlist) => {
+      foundlist.list.push(inputs);
+      foundlist.save();
+      res.redirect("/"+ listname);
+    });
+  }
+});
 
-
-app.listen(3000, function(){
-    console.log("Server is running on port 3000");
-})
+app.listen(process.env.PORT  || 3000, function () {
+  console.log("Server is running on port 3000");
+});
